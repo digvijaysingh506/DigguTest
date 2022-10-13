cd441a5c6978511b1e24be808f1dca5af96f09d4
digvijaysingh506 <37669212+digvijaysingh506@users.noreply.github.com>
2022-10-13 18:44:50 +0530

    Test Project
    Final Code

---
+++ src/main/java/com/example/demo/DigguTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigguTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigguTestApplication.class, args);
+		System.out.println("This Test Project for Azure Test");
+	}
+
+}
